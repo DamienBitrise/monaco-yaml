82aa3047497ba1fbdbd248642e627569bbbba34d
DamienBitrise <damien.murphy@bitrise.io>
2022-06-22 20:16:23 -0700

    Add Bitrise YAML Schema and GitClone Step Inputs as YAML Schema

--- examples/demo/src/index.ts
+++ examples/demo/src/index.ts
@@ -45,87 +45,595 @@ const defaultSchema: SchemasSettings = {
   fileMatch: ['monaco-yaml.yaml'],
 };
 
+const modelUri = Uri.parse('a://b/foo.yaml');
+
 setDiagnosticsOptions({
-  schemas: [defaultSchema],
+  enableSchemaRequest: true,
+  hover: true,
+  completion: true,
+  validate: true,
+  format: true,
+  schemas: [
+    {
+      uri: "http://placeholder/BitriseDataModel.json",
+      fileMatch: [String(modelUri)],
+      schema: {
+        "required": ["format_version"],
+        "properties": {
+          "format_version": {
+            "type": "string"
+          },
+          "default_step_lib_source": {
+            "type": "string"
+          },
+          "project_type": {
+            "type": "string"
+          },
+          "title": {
+            "type": "string"
+          },
+          "summary": {
+            "type": "string"
+          },
+          "description": {
+            "type": "string"
+          },
+          "app": {
+            "$ref": "http://placeholder/AppModel.json"
+          },
+          "meta": {
+            "patternProperties": {
+              ".*": {
+                "additionalProperties": true
+              }
+            },
+            "type": "object"
+          },
+          "trigger_map": {
+            "items": {
+              "$ref": "http://placeholder/TriggerMapItemModel.json"
+            },
+            "type": "array"
+          },
+          "pipelines": {
+            "patternProperties": {
+              ".*": {
+                "$ref": "http://placeholder/PipelineModel.json"
+              }
+            },
+            "type": "object"
+          },
+          "stages": {
+            "patternProperties": {
+              ".*": {
+                "$ref": "http://placeholder/StageModel.json"
+              }
+            },
+            "type": "object"
+          },
+          "workflows": {
+            "patternProperties": {
+              ".*": {
+                "$ref": "http://placeholder/WorkflowModel.json"
+              }
+            },
+            "type": "object"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/AppModel.json",
+      schema: {
+        "properties": {
+          "title": {
+            "type": "string"
+          },
+          "summary": {
+            "type": "string"
+          },
+          "description": {
+            "type": "string"
+          },
+          "envs": {
+            "items": {
+              "patternProperties": {
+                ".*": {
+                  "additionalProperties": true
+                }
+              },
+              "type": "object"
+            },
+            "type": "array"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/AptGetDepModel.json",
+      schema: {
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "bin_name": {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/BashStepToolkitModel.json",
+      schema: {
+        "properties": {
+          "entry_file": {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/BrewDepModel.json",
+      schema: {
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "bin_name": {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/CheckOnlyDepModel.json",
+      schema: {
+        "properties": {
+          "name": {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/DepsModel.json",
+      schema: {
+        "properties": {
+          "brew": {
+            "items": {
+              "$ref": "http://placeholder/BrewDepModel.json"
+            },
+            "type": "array"
+          },
+          "apt_get": {
+            "items": {
+              "$ref": "http://placeholder/AptGetDepModel.json"
+            },
+            "type": "array"
+          },
+          "check_only": {
+            "items": {
+              "$ref": "http://placeholder/CheckOnlyDepModel.json"
+            },
+            "type": "array"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/GoStepToolkitModel.json",
+      schema: {
+        "required": ["package_name"],
+        "properties": {
+          "package_name": {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/PipelineModel.json",
+      schema: {
+        "properties": {
+          "stages": {
+            "items": {
+              "patternProperties": {
+                ".*": {
+                  "$ref": "http://placeholder/StageModel.json"
+                }
+              },
+              "type": "object"
+            },
+            "type": "array"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/StageModel.json",
+      schema: {
+        "properties": {
+          "workflows": {
+            "items": {
+              "patternProperties": {
+                ".*": {
+                  "$ref": "http://placeholder/WorkflowModel.json"
+                }
+              },
+              "type": "object"
+            },
+            "type": "array"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/StepModel.json",
+      schema: {
+        "properties": {
+          "title": {
+            "type": "string"
+          },
+          "summary": {
+            "type": "string"
+          },
+          "description": {
+            "type": "string"
+          },
+          "website": {
+            "type": "string"
+          },
+          "source_code_url": {
+            "type": "string"
+          },
+          "support_url": {
+            "type": "string"
+          },
+          "published_at": {
+            "type": "string",
+            "format": "date-time"
+          },
+          "source": {
+            "$ref": "http://placeholder/StepSourceModel.json"
+          },
+          "asset_urls": {
+            "patternProperties": {
+              ".*": {
+                "type": "string"
+              }
+            },
+            "type": "object"
+          },
+          "host_os_tags": {
+            "items": {
+              "type": "string"
+            },
+            "type": "array"
+          },
+          "project_type_tags": {
+            "items": {
+              "type": "string"
+            },
+            "type": "array"
+          },
+          "type_tags": {
+            "items": {
+              "type": "string"
+            },
+            "type": "array"
+          },
+          "toolkit": {
+            "$ref": "http://placeholder/StepToolkitModel.json"
+          },
+          "deps": {
+            "$ref": "http://placeholder/DepsModel.json"
+          },
+          "is_requires_admin_user": {
+            "type": "boolean"
+          },
+          "is_always_run": {
+            "type": "boolean"
+          },
+          "is_skippable": {
+            "type": "boolean"
+          },
+          "run_if": {
+            "type": "string"
+          },
+          "timeout": {
+            "type": "integer"
+          },
+          "meta": {
+            "patternProperties": {
+              ".*": {
+                "additionalProperties": true
+              }
+            },
+            "type": "object"
+          },
+          "inputs": {
+            "items": {
+              "$ref": "http://placeholder/GitCloneModel.json"
+            },
+            "type": "array"
+          },
+          "outputs": {
+            "items": {
+              "patternProperties": {
+                ".*": {
+                  "additionalProperties": true
+                }
+              },
+              "type": "object"
+            },
+            "type": "array"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/GitCloneModel.json",
+      schema: {
+        "properties": {       
+          repository_url: {
+            "type": "string"
+          },
+          clone_into_dir: {
+            "type": "string"
+          },
+          commit: {
+            "type": "string"
+          },
+          tag: {
+            "type": "string"
+          },
+          branch: {
+            "type": "string"
+          },
+          branch_dest: {
+            "type": "string"
+          },
+          pull_request_id: {
+            "type": "string"
+          },
+          pull_request_repository_url: {
+            "type": "string"
+          },
+          pull_request_merge_branch: {
+            "type": "string"
+          },
+          pull_request_head_branch: {
+            "type": "string"
+          },
+          update_submodules: {
+            "type": "string"
+          },
+          clone_depth: {
+            "type": "string"
+          },
+          submodule_update_depth: {
+            "type": "string"
+          },
+          merge_pr: {
+            "type": "string"
+          },
+          sparse_directories: {
+            "type": "string"
+          },
+          reset_repository: {
+            "type": "string"
+          },
+          manual_merge: {
+            "type": "string"
+          },
+          fetch_tags: {
+            "type": "string"
+          },
+          build_url: {
+            "type": "string"
+          },
+          build_api_token: {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/StepSourceModel.json",
+      schema: {
+        "properties": {
+          "git": {
+            "type": "string"
+          },
+          "commit": {
+            "type": "string"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/StepToolkitModel.json",
+      schema: {
+        "properties": {
+          "bash": {
+            "$ref": "http://placeholder/BashStepToolkitModel.json"
+          },
+          "go": {
+            "$ref": "http://placeholder/GoStepToolkitModel.json"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/TriggerMapItemModel.json",
+      schema: {
+        "properties": {
+          "push_branch": {
+            "type": "string"
+          },
+          "pull_request_source_branch": {
+            "type": "string"
+          },
+          "pull_request_target_branch": {
+            "type": "string"
+          },
+          "tag": {
+            "type": "string"
+          },
+          "pipeline": {
+            "type": "string"
+          },
+          "workflow": {
+            "type": "string"
+          },
+          "pattern": {
+            "type": "string"
+          },
+          "is_pull_request_allowed": {
+            "type": "boolean"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    },
+    {
+      uri: "http://placeholder/WorkflowModel.json",
+      schema: {
+        "properties": {
+          "title": {
+            "type": "string"
+          },
+          "summary": {
+            "type": "string"
+          },
+          "description": {
+            "type": "string"
+          },
+          "before_run": {
+            "items": {
+              "type": "string"
+            },
+            "type": "array"
+          },
+          "after_run": {
+            "items": {
+              "type": "string"
+            },
+            "type": "array"
+          },
+          "envs": {
+            "items": {
+              "patternProperties": {
+                ".*": {
+                  "additionalProperties": true
+                }
+              },
+              "type": "object"
+            },
+            "type": "array"
+          },
+          "steps": {
+            "items": {
+              "patternProperties": {
+                ".*": {
+                  "$ref": "http://placeholder/StepModel.json"
+                }
+              },
+              "type": "object"
+            },
+            "type": "array"
+          },
+          "meta": {
+            "patternProperties": {
+              ".*": {
+                "additionalProperties": true
+              }
+            },
+            "type": "object"
+          }
+        },
+        "additionalProperties": false,
+        "type": "object"
+      }
+    }
+  ],
 });
 
-const value = `
-# Property descriptions are displayed when hovering over properties using your cursor
-property: This property has a JSON schema description
-
-
-# Titles work too!
-titledProperty: Titles work too!
-
-
-# Even markdown descriptions work
-markdown: hover me to get a markdown based description 😮
-
-
-# Enums can be autocompleted by placing the cursor after the colon and pressing Ctrl+Space
-enum:
-
-
-# Unused anchors will be reported
-unused anchor: &unused anchor
-
-
-# Of course numbers are supported!
-number: 12
-
-
-# As well as booleans!
-boolean: true
-
-
-# And strings
-string: I am a string
-
-
-# This property is using the JSON schema recursively
-reference:
-  boolean: Not a boolean
-
-
-# Also works in arrays
-array:
-  - string: 12
-    enum: Mewtwo
-    reference:
-      reference:
-        boolean: true
-
-
-# JSON referenses can be clicked for navigation
-pointer:
-  $ref: '#/array'
-
-
-# This anchor can be referenced
-anchorRef: &anchor can be clicked as well
-
-
-# Press control while hovering over the anchor
-anchorPointer: *anchor
-
-
-formatting:       Formatting is supported too! Under the hood this is powered by Prettier. Just press Ctrl+Shift+I or right click and press Format to format this document.
-
-
-
-
-
-
+const value = `---
+format_version: '11'
+default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+project_type: ios
+trigger_map:
+- pull_request_source_branch: "*"
+  workflow: primary
+workflows:
+  primary:
+    steps:
+    - activate-ssh-key@4:
+        run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+    - git-clone@4: {}
+    - cache-pull@2: {}
+    - script@1:
+        inputs:
+        - content: for ip in $(dig @8.8.8.8 github.com +short); do ssh-keyscan github.com,$ip;
+            ssh-keyscan $ip; done 2>/dev/null >> ~/.ssh/known_hosts
+        title: Saving to known_hosts
+app:
+  envs:
+  - opts:
+      is_expand: false
+    BITRISE_PROJECT_PATH: STORESPaymentsSDK.xcodeproj
+  - opts:
+      is_expand: false
+    BITRISE_SCHEME: STORESPaymentsSDK
+  - opts:
+      is_expand: false
+    BITRISE_EXPORT_METHOD: development
+  - opts:
+      is_expand: false
+    BITRISE_SCHEME_INTERNAL_SDK: STPInternalSDK
+meta:
+  bitrise.io:
+    stack: osx-xcode-13.3.x
 `.replace(/:$/m, ': ');
 
 const ed = editor.create(document.getElementById('editor'), {
   automaticLayout: true,
-  model: editor.createModel(value, 'yaml', Uri.parse('monaco-yaml.yaml')),
+  model: editor.createModel(value, 'yaml', modelUri),
   theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'vs-dark' : 'vs-light',
 });
 
 const select = document.getElementById('schema-selection') as HTMLSelectElement;
-
+/*
 fetch('https://www.schemastore.org/api/json/catalog.json').then(async (response) => {
   if (!response.ok) {
     return;
@@ -159,10 +667,11 @@ fetch('https://www.schemastore.org/api/json/catalog.json').then(async (response)
     schemas,
   });
 });
+*/
 
 select.addEventListener('change', () => {
   const oldModel = ed.getModel();
-  const newModel = editor.createModel(oldModel.getValue(), 'yaml', Uri.parse(select.value));
+  const newModel = editor.createModel(oldModel.getValue(), 'yaml', modelUri);
   ed.setModel(newModel);
   oldModel.dispose();
 });
